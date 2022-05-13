3d3ca2157e24b6bd6ba6c00373ffc4c7f45e8ae9
Alexander <aleprada_ar@hotmail.com>
2022-05-13 15:24:05 -0300

    Actualizo dependencias del generador

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,9 +1,9 @@
 'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+import Generator from 'yeoman-generator';
+import chalk from 'chalk';
+import yosay from 'yosay';
 
-module.exports = class extends Generator {
+export default class GeneratorGrava extends Generator {
   prompting() {
     this.log(
       yosay(`Welcome to the terrific ${chalk.red('generator-grava-api')} generator!`)
