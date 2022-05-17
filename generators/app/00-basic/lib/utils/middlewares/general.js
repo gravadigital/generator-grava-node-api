7edd069c0dfe7de974dfbf18df619ebcf22e9a95
Alexander <aleprada_ar@hotmail.com>
2022-05-17 09:36:55 -0300

    Cambio de lugar el middleware general a la estructura base, borro función duplicada

--- generators/app/00-basic/lib/utils/middlewares/general.js
+++ generators/app/00-basic/lib/utils/middlewares/general.js
@@ -14,23 +14,6 @@ function validateBodyFields(validationSchema) {
     };
 }
 
-function validateBodyFields(validationSchema) {
-    return (req, res, next) => {
-        const {error} = validationSchema.validate(req.body);
-
-        if (error) {
-            const firstError = error.details[0].message;
-            return res.status(400).json({
-                code: 'invalid_fields',
-                message: `Invalid field - ${firstError}`
-            });
-        }
-
-        return next();
-    };
-}
-
-
 module.exports = {
     validateBodyFields
 }
